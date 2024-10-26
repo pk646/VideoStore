ecfa335fb903596fd9e92f225bebb7d2bbce908d
Pavan Kumar VADDE <pkvadde@justransform.com>
2024-10-26 20:09:54 +0530

    Add project files.

---
+++ VideoStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VideoStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
