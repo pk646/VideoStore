35c15d24cb45d9eea69a9515bcdaa375ee4e5836
Pavan Kumar VADDE <pkvadde@justransform.com>
2024-10-26 22:23:43 +0530

    started to build Video Store WebApp

---
+++ VideoStore/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoStore.Models;
+
+namespace VideoStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
